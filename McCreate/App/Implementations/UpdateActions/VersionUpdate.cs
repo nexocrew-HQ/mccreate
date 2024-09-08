98a28b572b7b23efb8f5b3163903f20d4b63b792
Moritz-Deiaco <moritz.deiaco@gmail.com>
2024-09-08 11:02:00 +0200

    fixed bug where server.jar corrupts after changing the version

--- McCreate/App/Implementations/UpdateActions/VersionUpdate.cs
+++ McCreate/App/Implementations/UpdateActions/VersionUpdate.cs
@@ -2,6 +2,7 @@ using McCreate.App.Extensions;
 using McCreate.App.Helpers;
 using McCreate.App.Interfaces;
 using McCreate.App.Models;
+using MoonCore.Helpers;
 using Spectre.Console;
 using Version = McCreate.App.Models.Version;
 
@@ -39,6 +40,31 @@ public class VersionUpdate : IUpdateAction
         AnsiHelper.ConfirmSelection("Selected subversion", server.Version.Name);
         
         await ServerHelper.UpdateServer(server, server.Path, serviceProvider);
+
+        var jarPath = PathBuilder.File(server.Path, "server.jar");
+
+        try
+        {
+            File.Delete(jarPath);
+        }
+        catch (Exception e)
+        {
+            switch (e)
+            {
+                case DirectoryNotFoundException:
+                    AnsiHelper.Error($"The directory of the server ({server.Path}) wasn't found, aborting.");
+                    return;
+                case IOException:
+                    AnsiHelper.Error($"The file ({server.Path}/server.jar) is currently used by another process, aborting.");
+                    return;
+                default:
+                    AnsiHelper.Error($"An unknown error occured, aborting.");
+                    AnsiHelper.Error(e.Message);
+                    if (e.StackTrace != null)
+                        AnsiHelper.Error(e.StackTrace);
+                    return;
+            }
+        }
         
         await AnsiConsole.Status()
             .Spinner(Spinner.Known.Line)
