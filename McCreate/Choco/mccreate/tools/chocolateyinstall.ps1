ff6cc13218668df86e2ab3f55d6b32e38b420e45
Jesper <jesper.richert@web.de>
2024-09-25 21:31:13 +0200

    Commit some Files

--- McCreate/Choco/mccreate/tools/chocolateyinstall.ps1
+++ McCreate/Choco/mccreate/tools/chocolateyinstall.ps1
@@ -10,21 +10,21 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 # Internal packages (organizations) or software that has redistribution rights (community repo)
 # - Use `Install-ChocolateyInstallPackage` instead of `Install-ChocolateyPackage`
 #   and put the binaries directly into the tools folder (we call it embedding)
-#$fileLocation = Join-Path $toolsDir 'NAME_OF_EMBEDDED_INSTALLER_FILE'
+$fileLocation = Join-Path $toolsDir 'mccreate.exe'
 # If embedding binaries increase total nupkg size to over 1GB, use share location or download from urls
 #$fileLocation = '\\SHARE_LOCATION\to\INSTALLER_FILE'
 # Community Repo: Use official urls for non-redist binaries or redist where total package size is over 200MB
 # Internal/Organization: Download from internal location (internet sources are unreliable)
-$url        = 'https://installer.system.nexocrew.space/mccreate/win/mccreate.exe' # download url, HTTPS preferred
-$url64      = '' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+# $url        = '' # download url, HTTPS preferred
+ $url64      = '' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE' #only one of these: exe, msi, msu
-  url           = $url
-  url64bit      = $url64
-  #file         = $fileLocation
+  #url           = $url
+  #url64bit      = $url64
+  file         = $fileLocation
 
   softwareName  = 'mccreate*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
